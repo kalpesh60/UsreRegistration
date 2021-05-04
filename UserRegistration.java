1592ff76923eb7ec43d6df60931c8a76d4441075
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-05-04 22:19:27 +0530

    Added:code for Email Id

--- UserRegistration.java
+++ UserRegistration.java
@@ -18,9 +18,17 @@ public class UserRegistration {
         System.out.println("The name is "+lastName);
     }
 
+    public void emailValidation() {
+        System.out.println("Enter Email Id");
+        String mail = sc.nextLine();
+        boolean email = Pattern.compile("^[A-Za-z0-9]{1,}[-+_.]{0,1}[A-Za-z0-9]{0,}[@]{1}[A-Za-z0-9]{1,}[.]{1}[A-Za-z]{2,3}[.]{0,1}[A-Za-z]{0,3}").matcher(mail).matches();
+        System.out.println("The Email is "+email);
+    }
+
     public static void main(String args[]){
         UserRegistration user = new UserRegistration();
         user.firstNameValidation();
         user.lastNameValidation();
+        user.emailValidation();
     }
 }
