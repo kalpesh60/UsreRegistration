6f73fa36cd77df5bf2e7ce3ec74b942d45df4bbd
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-05-04 22:00:33 +0530

    Added:code for first Name

---
+++ UserRegistration.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+    Scanner sc = new Scanner(System.in);
+
+    public void firstNameValidation() {
+        System.out.println("Enter First Name");
+        String name = sc.nextLine();
+        boolean firstName = Pattern.compile("^[A-Z]{1}[a-zA-Z]{3,}").matcher(name).matches();
+        System.out.println("The name is "+firstName);
+    }
+    public static void main(String args[]){
+        UserRegistration user = new UserRegistration();
+        user.firstNameValidation();
+    }
+}
